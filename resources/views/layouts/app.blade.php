81f1d9b7a460bdfb01a1eef9d40046e61185ffb5
Mckenzie Arts <monneylobe@gmail.com>
2018-06-27 17:03:04 +0100

    Update app default layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -160,5 +160,6 @@
 
     <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
     <script src="{{ Asset::path('application.js') }}"></script>
+    @stack('scripts')
 </body>
 </html>
