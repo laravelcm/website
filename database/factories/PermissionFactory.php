7fd1a4088594449d2922d2ff57145c79f757940d
Arthur Monney <monneylobe@gmail.com>
2020-01-17 22:23:47 +0100

    :card_file_box: Update factories

--- database/factories/PermissionFactory.php
+++ database/factories/PermissionFactory.php
@@ -1,5 +1,5 @@
 <?php
-
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 use Faker\Generator as Faker;
 
 $factory->define(\Spatie\Permission\Models\Permission::class, function (Faker $faker) {
--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -1,5 +1,5 @@
 <?php
-
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 use Faker\Generator as Faker;
 
 $factory->define(\Modules\User\Entities\Role::class, function (Faker $faker) {
