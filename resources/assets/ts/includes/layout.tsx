8ac4e7db7c03264fbb59bbe89d8df5fc7293a62c
Arthur Monney <monneylobe@gmail.com>
2020-01-22 23:34:37 +0100

    :lipstick: Add responsive menu to layout

--- resources/assets/ts/includes/layout.tsx
+++ resources/assets/ts/includes/layout.tsx
@@ -16,7 +16,7 @@ export default ({ children }: LayoutProps) => {
         meta={[]}
       />
       <Header />
-      <main className="py-18 w-full">{children}</main>
+      <main className="py-18 w-full min-h-screen w-full lg:static lg:max-h-full lg:overflow-visible">{children}</main>
       <Footer />
     </>
   );
