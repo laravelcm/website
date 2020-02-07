ffc61292e14e51e2831f4b1f52954162af5788d7
Arthur Monney <monneylobe@gmail.com>
2020-02-07 02:21:37 +0100

    :rocket: Deploy a first version for website

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\LanguageController;
 
 // Switch between the included languages
 Route::get('lang/{lang}', [LanguageController::class, 'swap']);
+// Route::get('/')->name('frontend.index')->uses('HomeController');
 
-Route::get('/')->name('frontend.index')->uses('HomeController');
-Route::get('/welcome', function () {
+Route::get('/', function () {
     return view('welcome');
 });
