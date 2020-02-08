d2bfc0d65adc836702e57f9367e96c96bc685a55
Arthur Monney <monneylobe@gmail.com>
2020-02-08 21:47:48 +0100

    :wrench: Update mail config and setup Laravel Cameroun infos

--- config/mail.php
+++ config/mail.php
@@ -55,8 +55,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@laravelcm.com'),
+        'name' => env('MAIL_FROM_NAME', 'Laravel Cameroun'),
     ],
 
     /*
