0a8f29ecc46814de7fa9e1577128354ea26490d9
Arthur Monney <monneylobe@gmail.com>
2020-04-08 15:16:30 +0100

    :construction: Page d'erreur

--- resources/assets/ts/pages/error/Index.tsx
+++ resources/assets/ts/pages/error/Index.tsx
@@ -1,30 +1,43 @@
 import React from "react";
 import Helmet from "react-helmet";
 
-export default ({ status }: { status: any }) => {
+import Layout from "@/includes/Layout";
+
+const Error = ({ status }: { status: any }) => {
   // @ts-ignore
   const title = {
     503: "503: Service Unavailable",
     500: "500: Server Error",
-    404: "404: Page Not Found",
-    403: "403: Forbidden",
+    404: "404: Introuvable",
+    403: "403: Permission",
   }[status];
 
   // @ts-ignore
   const description = {
-    503: "Sorry, we are doing some maintenance. Please check back soon.",
-    500: "Whoops, something went wrong on our servers.",
-    404: "Sorry, the page you are looking for could not be found.",
-    403: "Sorry, you are forbidden from accessing this page.",
+    503: "Désolé, Nous effectuons une maintenance. Revenez plus tard.",
+    500: "Whoops, Quelque chose s'est passé sur nos serveurs.",
+    404: "Wooo 😱, La ressource que vous demandez est introuvable.",
+    403: "🔒, Vous ne pouvez pas accéder a cette ressource.",
   }[status];
 
   return (
-    <div className="p-5 bg-brand-primary text-indigo-100 min-h-screen flex justify-center items-center">
+    <>
       <Helmet title={title} />
-      <div className="w-full max-w-md">
-        <h1 className="text-3xl">{title}</h1>
-        <p className="mt-3 text-lg leading-tight">{description}</p>
+      <div className="flex items-center justify-center">
+        <div className="w-full max-w-7xl flex items-center px-4 sm:px-6 py-10 lg:py-20">
+          <div className="max-w-lg">
+            <h1 className="text-base sm:text-lg md:text-3xl lg:text-4xl xl:text-5xl font-medium leading-5">{title}</h1>
+            <p className="mt-8 text-lg lg:text-xl leading-tight">{description}</p>
+          </div>
+          <div className="hidden md:block ml-6">
+            Illustration
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
+
+Error.layout = (page: React.ReactNode) => <Layout child={page} />;
+
+export default Error;
--- resources/assets/ts/pages/home/Index.tsx
+++ resources/assets/ts/pages/home/Index.tsx
@@ -4,7 +4,7 @@ import { InertiaLink, usePage } from "@inertiajs/inertia-react";
 import Layout from "@/includes/Layout";
 import Seo from "@/includes/Seo";
 
-// import Tutorial from "@/components/Tutorial";
+import Tutorial from "@/components/Tutorial";
 import Post from "@/components/Post";
 import { PostType, ThreadType } from "@/utils/types";
 
@@ -124,15 +124,13 @@ const Home = () => {
             </span>
           </InertiaLink>
         </div>
-        {/* eslint-disable-next-line max-len */}
-        {/* <h2 className="text-gray-700 mb-6 text-2xl font-medium lg:text-3xl">Les derniers tutoriels</h2> */}
-        {/* eslint-disable-next-line max-len */}
-        {/* <div className="space-y-5 mb-10 md:space-y-0 md:flex md:-mx-4 flex-grow flex-wrap lg:mb-14"> */}
-        {/*  <Tutorial */}
-        {/*    image="https://cdn.devdojo.com/posts/images/June2019/laravel-on-digital-ocean.jpg?auto=compress&w=228&h=128&dpr=2" */}
-        {/*    title="Laravel App on Digital Ocean Ubuntu 19.04 droplet (Step by Step Guide)" */}
-        {/*  /> */}
-        {/* </div> */}
+        <h2 className="text-gray-700 mb-6 text-2xl font-medium lg:text-3xl">Les derniers tutoriels</h2>
+        <div className="grid gap-6 max-w-lg mx-auto md:grid-cols-2 md:max-w-3xl lg:grid-cols-3 lg:max-w-none sm:mb-18">
+          <Tutorial
+            image="https://cdn.devdojo.com/posts/images/June2019/laravel-on-digital-ocean.jpg?auto=compress&w=228&h=128&dpr=2"
+            title="Laravel App on Digital Ocean Ubuntu 19.04 droplet (Step by Step Guide)"
+          />
+        </div>
         <h2 className="text-gray-700 mb-6 text-2xl font-medium lg:text-3xl">Les derniers articles</h2>
         <div className="grid gap-6 max-w-lg mx-auto md:grid-cols-2 md:max-w-3xl lg:grid-cols-3 lg:max-w-none sm:mb-18">
           {posts.map((post: PostType) => <Post key={post.id} post={post} />)}
@@ -142,9 +140,7 @@ const Home = () => {
         <div className="container">
           <div className="space-y-8 lg:space-y-0 lg:space-x-6 lg:flex flex-grow">
             <div className="w-full lg:w-1/3">
-              <h4 className="font-medium text-lg mb-4 lg:mb-8">
-                Dernières offres d'emploi
-              </h4>
+              <h4 className="font-medium text-lg mb-4 lg:mb-8">Dernières offres d'emploi</h4>
               <ul>
                 <li className="py-3">
                   <InertiaLink href="/" className="flex items-center hover:text-gray-200">
