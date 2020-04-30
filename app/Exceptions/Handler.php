eb718e0f95c8479f3009ad941d297db19c4785f6
Arthur Monney <monneylobe@gmail.com>
2020-04-30 08:55:46 +0100

    :bug: Fix du bug sur Laravel Sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -40,10 +40,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
-        if (app()->environment('production') &&
-            (app()->bound('sentry') && $this->shouldReport($exception))
-        )
-        {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
             app('sentry')->captureException($exception);
         }
 
