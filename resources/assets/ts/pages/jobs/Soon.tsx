38597edc1b178c5f14733b8f4f0432c778b3d6ff
Arthur Monney <monneylobe@gmail.com>
2020-05-01 13:33:03 +0100

    :bento: Modification des assets

--- resources/assets/ts/pages/jobs/Soon.tsx
+++ resources/assets/ts/pages/jobs/Soon.tsx
@@ -81,7 +81,7 @@ const Soon = () => (
       </div>
     </div>
     <div>
-      <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+      <div className="relative max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <img className="h-20" src={require("@/assets/brands/iug-douala.svg")} alt="IUG Douala" />
