7ea20703a22a5c68f1fb43f5bec1cdc749092e28
Arthur Monney <monneylobe@gmail.com>
2020-05-04 20:40:45 +0100

    :art: Suppression du middleware de la demo

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \App\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware\CheckForDemoMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
